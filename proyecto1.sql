ea6bf12a8c9ec856324beb32262c941b74804049
gastonbarbaccia <gastonbarbaccia@hotmail.com>
2022-10-14 22:34:02 -0300

    Se modifica el script de base de datos

--- proyecto1.sql
+++ proyecto1.sql
@@ -18,6 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+DROP DATABASE `proyecto1`;
 --
 -- Base de datos: `proyecto1`
 --
