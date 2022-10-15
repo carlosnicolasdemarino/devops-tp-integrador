696a9103e2f58b53f8b6972b55705f0fd0cfde1f
carlos nicolas <109827143+carlosnicolasdemarino@users.noreply.github.com>
2022-10-15 14:08:33 -0300

    Update conexiondb.php

--- conexiondb.php
+++ conexiondb.php
@@ -4,9 +4,9 @@
 //funcion que se encargar de crear la conexion de la base de datos 
 function conexiondb(){
 	//datos del servidor y de la base de datos
-	$localhost = "localhost";
-	$username = "root";
-	$password = "";
+	$localhost = "devops-tp-integrador.capjeqwtb4vf.us-east-2.rds.amazonaws.com";
+	$username = "admin";
+	$password = "Inicio123.";
 	$database = "proyecto1";
 
 	// crea la conexion con los datos pasados en la funcion mysqli()
